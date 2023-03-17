f181b42b726cc3099299c5ccd79210494c496c37
GerardoLT <120034634@upq.edu.mx>
2023-03-17 15:30:43 -0600

    nc

---
+++ servicios/testeo/views.py
@@ -0,0 +1,29 @@
+import json
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from logica import users
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+# Create your views here.
+def index(request):
+    data=json.loads(request.body.decode('utf-8'))
+    nick_name = data['nick_name']
+    full_name = data['full_name']
+    
+    try:
+        user = users.create_user(nick_name=nick_name, full_name=full_name)
+        response ={
+            "status":200,
+            "code": 200,
+            "data":"User se guardo correctamente"
+        }
+    except Exception as e:
+        response ={
+            "status": "Error",
+            "code": 400,
+            "data": str(e)
+        }
+    return JsonResponse(response)
